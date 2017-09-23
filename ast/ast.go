c7cb0621a404483f30196659ea8212132865ed19
Julson Lim <julsonlim@gmail.com>
2017-09-24 03:59:16 +0800

    Parse boolean literals

--- ast/ast.go
+++ ast/ast.go
@@ -149,6 +149,15 @@ func (oe *InfixExpression) String() string {
 	return out.String()
 }
 
+type Boolean struct {
+	Token token.Token
+	Value bool
+}
+
+func (b *Boolean) expressionNode()      {}
+func (b *Boolean) TokenLiteral() string { return b.Token.Literal }
+func (b *Boolean) String() string       { return b.Token.Literal }
+
 func (p *Program) TokenLiteral() string {
 	if len(p.Statements) > 0 {
 		return p.Statements[0].TokenLiteral()
